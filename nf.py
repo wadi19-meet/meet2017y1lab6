7f4100f8cd95ecfe05c97064a6b04bd467f93311
wadi19-meet <wadi19@meet.mit.edu>
2017-07-31 18:40:08 +0300

    Stopping here

--- nf.py
+++ nf.py
@@ -1,25 +1,70 @@
 
 import turtle
+turtle.shape('turtle')
 def up():
     global direction
     direction=UP
     print('you moved up')
+
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y+10)
+    print(turtle.pos())
+
+
+
+
+
+
+
 def down():
     global direction
     direction=DOWN
     print('you moved down')
+
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y-10)
+    print(turtle.pos())
+
+
+
+
+
+
 def left():
     global direction
     direction=LEFT
     print('you moved left')
+
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x-10,y)
+    print(turtle.pos())
+
+
+
+
+
+
 def right():
     global direction
     direction=RIGHT
     print('you moved right')
 
 
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x+10,y)
+    print(turtle.pos())
 
-
+def spacebar():
+    turtle.shape('turtle')
+    turtle.stamp()
 #################################################################
 
 
@@ -35,6 +80,7 @@ UP=0
 DOWN=1
 LEFT=2
 RIGHT=3
+
 direction=UP
 
 ##turtle.shape('turtle')
@@ -47,6 +93,11 @@ turtle.onkeypress(up,UP_ARROW)
 turtle.onkeypress(down,DOWN_ARROW)
 turtle.onkeypress(left,LEFT_ARROW)
 turtle.onkeypress(right,RIGHT_ARROW)
+turtle.onkeypress(spacebar,SPACEBAR)
 
-turtle.onkeypress
-turtle.onkeypress
+turtle.listen()
+
+
+turtle.mainloop()
+
+
